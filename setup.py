9856c1e93a10e6b09395cbab68ff44866a06f91f
Florian Pilz <fp@gocept.com>
2014-11-14 12:10:03 +0100

    Back to development: 2.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 NEWS = open(os.path.join(here, 'CHANGES.txt')).read()
 
 
-version = '2.2.2'
+version = '2.2.3.dev0'
 
 install_requires = [
     'gocept.cache',
