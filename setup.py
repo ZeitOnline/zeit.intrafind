4a2cdcceb8d29c7cbd1ffa323ab04abb96b48c1f
Wolfgang Schnerring <ws@gocept.com>
2015-01-08 17:02:44 +0100

    Update copyright

--- setup.py
+++ setup.py
@@ -1,39 +1,26 @@
 from setuptools import setup, find_packages
-import os
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-NEWS = open(os.path.join(here, 'CHANGES.txt')).read()
-
-
-version = '2.2.3.dev0'
-
-install_requires = [
-    'gocept.cache',
-    'gocept.lxml',
-    'mock',
-    'setuptools',
-    'zeit.cms >= 2.20.0.dev0',
-    'zope.interface',
-    'zope.schema',
-]
 
 
 setup(
     name='zeit.intrafind',
-    version=version,
+    version='2.2.3.dev0',
+    author='gocept, Zeit Online',
+    author_email='zon-backend@zeit.de',
+    url='http://www.zeit.de/',
     description="vivi intrafind integration",
-    long_description=README + '\n\n' + NEWS,
-    classifiers=[],
-    keywords='',
-    author='gocept',
-    author_email='mail@gocept.com',
-    url='',
-    license='ZPL 2.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    namespace_packages=['zeit'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
+    license='BSD',
+    namespace_packages=['zeit'],
+    install_requires=[
+        'gocept.cache',
+        'gocept.lxml',
+        'mock',
+        'setuptools',
+        'zeit.cms >= 2.20.0.dev0',
+        'zope.interface',
+        'zope.schema',
+    ]
 )
--- src/zeit/intrafind/testing.py
+++ src/zeit/intrafind/testing.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2011 gocept gmbh & co. kg
-# See also LICENSE.txt
-
 import gocept.httpserverlayer.custom
 import pkg_resources
 import zeit.cms.testing
